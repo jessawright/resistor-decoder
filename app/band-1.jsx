a0a9e6ec83c6db3b18b747fed796071684493c2c
Jessa Wright <jessa.wright@gmail.com>
2017-01-26 16:44:57 -0600

    --why won't my svg use element work?

--- app/band-1.jsx
+++ app/band-1.jsx
@@ -12,11 +12,9 @@ class Band1 extends React.Component {
       <g>
         <path id="band-1" className={this.props.bandColor} d="m74.05 0.049894v170h29.9v-170h-29.9z"/>
         <defs>
-          <g id="help" x="104" y="40" width="100" height="100">
-            <foreignObject >
-              <BandColorMenu resistorDecoderInfo={this.props.resistorDecoderInfo} selectedBandNumber={this.props.selectedBandNumber} bandThisIs='band1'/>
-            </foreignObject>
-          </g>
+          <foreignObject id="help" x="0" y="0" height="100" width="1000" >
+            <BandColorMenu resistorDecoderInfo={this.props.resistorDecoderInfo} selectedBandNumber={this.props.selectedBandNumber} bandThisIs='band1'/>
+          </foreignObject>
         </defs>
       </g>
 
