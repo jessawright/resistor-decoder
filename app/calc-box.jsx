e450ba46ac910b4d5a2e66e7562904b81c544a9c
Jessa Wright <jessa.wright@gmail.com>
2017-02-07 13:25:01 -0600

    automatically displays ohms/Kohms/Mohms instead of only ohms

--- app/calc-box.jsx
+++ app/calc-box.jsx
@@ -16,7 +16,13 @@ class CalcBox extends React.Component {
     function displayValues() {
       switch (id) {
         case 'digit-box':
+        if ((decodedResistor.digits.join('') * decodedResistor['multiplier']) >= 1000000) {
+          boxContent = ((decodedResistor.digits.join('') * decodedResistor['multiplier']) / 1000000) + 'M' + units['multiplier'];
+        } else if ((decodedResistor.digits.join('') * decodedResistor['multiplier']) >= 1000) {
+          boxContent = ((decodedResistor.digits.join('') * decodedResistor['multiplier']) / 1000) + 'K' + units['multiplier'];
+        } else {
         boxContent = (decodedResistor.digits.join('') * decodedResistor['multiplier']) + ' ' + units['multiplier'];
+      };
         break;
         case 'tolerance-box':
         boxContent = decodedResistor['tolerance'] + ' ' + units['tolerance'];
