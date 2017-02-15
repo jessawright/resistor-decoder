d7eda5e5fc93e581e84af8f61584ab220b03c3d5
Jessa Wright <jessa.wright@gmail.com>
2017-02-15 16:37:38 -0600

    --tinkering with webpack for image

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 var webpackSettings = {
  entry: './app/app.jsx',
  output: {
-  publicPath: '/',
+  publicPath: '/resistor-decoder/',
   path: './site',
   filename: '/js/app.js'
  },
