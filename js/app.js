21f20cde020dda4acced1ec1d6a0786ed3fae06c
Jessa Wright <jessa.wright@gmail.com>
2017-02-15 16:46:50 -0600

    --tinkering with webpack for image

--- js/app.js
+++ js/app.js
@@ -34,7 +34,7 @@
 /******/ 	__webpack_require__.c = installedModules;
 
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/resistor-decoder/";
+/******/ 	__webpack_require__.p = "https://jessawright.github.io/resistor-decoder/";
 
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(0);
@@ -21956,7 +21956,7 @@
 	              _react2.default.createElement("rect", { x: "0", y: "0", width: "8", height: "8", fill: "#555" }),
 	              _react2.default.createElement("rect", { x: "0", y: "0", width: "4", height: "8", stroke: "none", fill: "#222" })
 	            ),
-	            _react2.default.createElement("image", { id: "glitter-texture", xlinkHref: "../assets/images/egg-shell.png", x: "0", y: "0", width: "256", height: "256" }),
+	            _react2.default.createElement("image", { id: "glitter-texture", xlinkHref: "../resistor-decoder/assets/images/egg-shell.png", x: "0", y: "0", width: "256", height: "256" }),
 	            _react2.default.createElement(
 	              "pattern",
 	              { id: "gold-glitter", patternUnits: "userSpaceOnUse", width: "256", height: "256" },
